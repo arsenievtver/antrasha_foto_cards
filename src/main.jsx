f5925d1767d2f291026e3101523385c0e73c7510
i@aarsenev.ru <i@aarsenev.ru>
2026-08-13 19:47:54 +0300

    фиксы главной страницы 3

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { bindAppVh } from './utils/appVh.js'
 import './index.css'
 import App from './App.jsx'
+
+bindAppVh()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
